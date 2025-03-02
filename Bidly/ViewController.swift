8d26fdd03274509145a091368f8f234c99422128
kanaevbaytik <kanaevbaytik@gmail.com>
2025-03-03 01:25:12 +0600

    Initial Commit

---
+++ Bidly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bidly
+//
+//  Created by Baytik  on 3/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
